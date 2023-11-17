9468128b1f2642d952e421a7fc7da1e1c56c1872
Tiago Angelo <tiago.angelo@totvs.com.br>
2023-11-17 14:19:39 -0300

    Add EmptyList function and implement methods for empty list

--- list.go
+++ list.go
@@ -18,6 +18,65 @@ type List interface {
 	Append(mfs ...List) List
 }
 
+func EmptyList() List {
+	return new(empty)
+}
+
+type empty struct{}
+
+func (e *empty) Delete(ctx context.Context) error {
+	return nil
+}
+
+func (e *empty) Apply(ctx context.Context) error {
+	return nil
+}
+
+func (e *empty) Filter(funcs ...Filter) List {
+	return e
+}
+
+func (e *empty) Transform(funcs ...Transformer) (List, error) {
+	return e, nil
+}
+
+func (e *empty) Resources() []*unstructured.Unstructured {
+	return nil
+}
+
+func (e *empty) Size() int {
+	return 0
+}
+
+func (e *empty) Append(mfs ...List) List {
+	resources := make([]*unstructured.Unstructured, 0)
+
+	for _, mf := range mfs {
+		for _, v := range mf.Resources() {
+			resource := v.DeepCopy()
+			resources = append(resources, resource)
+		}
+	}
+
+	var (
+		fieldManager string
+		client       dynamic.Interface
+		mapper       meta.RESTMapper
+	)
+
+	for _, mf := range mfs {
+		if l, ok := mf.(*list); ok {
+			fieldManager = l.fieldManager
+			client = l.client
+			mapper = l.mapper
+
+			break
+		}
+	}
+
+	return &list{resources: resources, fieldManager: fieldManager, client: client, mapper: mapper}
+}
+
 type list struct {
 	resources    []*unstructured.Unstructured
 	fieldManager string
