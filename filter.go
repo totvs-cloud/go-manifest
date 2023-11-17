d9aa78e96fdacc2cabea89f0003d78b0e1638225
Tiago Angelo <tiago.angelo@totvs.com.br>
2023-11-17 15:23:38 -0300

    Add filter functions for API version, kind, annotation, label and labels

--- filter.go
+++ filter.go
@@ -18,3 +18,79 @@ func (l *list) Filter(funcs ...Filter) List {
 
 	return &list{resources: resources, fieldManager: l.fieldManager, client: l.client, mapper: l.mapper}
 }
+
+func All(filters ...Filter) Filter {
+	return func(u *unstructured.Unstructured) bool {
+		for _, filter := range filters {
+			if !filter(u) {
+				return false
+			}
+		}
+
+		return true
+	}
+}
+
+func Any(filters ...Filter) Filter {
+	return func(u *unstructured.Unstructured) bool {
+		for _, filter := range filters {
+			if filter(u) {
+				return true
+			}
+		}
+
+		return false
+	}
+}
+
+func Not(filter Filter) Filter {
+	return func(u *unstructured.Unstructured) bool {
+		return !filter(u)
+	}
+}
+
+func ByAPIVersion(apiVersion string) Filter {
+	return func(u *unstructured.Unstructured) bool {
+		return u.GetAPIVersion() == apiVersion
+	}
+}
+
+func ByKind(kind string) Filter {
+	return func(u *unstructured.Unstructured) bool {
+		return u.GetKind() == kind
+	}
+}
+
+func ByAnnotation(annotation, value string) Filter {
+	return func(u *unstructured.Unstructured) bool {
+		v, ok := u.GetAnnotations()[annotation]
+		if value == "" {
+			return ok
+		}
+
+		return v == value
+	}
+}
+
+func ByLabel(label, value string) Filter {
+	return func(u *unstructured.Unstructured) bool {
+		v, ok := u.GetLabels()[label]
+		if value == "" {
+			return ok
+		}
+
+		return v == value
+	}
+}
+
+func ByLabels(labels map[string]string) Filter {
+	return func(u *unstructured.Unstructured) bool {
+		for key, value := range labels {
+			if v := u.GetLabels()[key]; v == value {
+				return true
+			}
+		}
+
+		return false
+	}
+}
